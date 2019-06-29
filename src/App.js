735fdefda6ff6ad587e7dd921f6ae136be961ac4
Sergei Mikhailovskikh <s.mikhailovskikh@volsor.com>
2019-06-29 10:14:15 +0200

    first version

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function App () {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>
+                    How many CV have you looked through before came here?
+                    <br/>
+                    May be this one is what you are looking for?
+                </h1>
+                <br/>
+                <p>
+                    Curious?
+                    <br/>
+                    <br/>
+                    <a className="dbox-donation-button"
+                       href="https://donorbox.org/my-cv?hide_donation_meter=true">
+                        FIND OUT NOW
+                    </a>
+                    <br/>
+                    <br/>
+                    for.mikhe@gmail.com
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
