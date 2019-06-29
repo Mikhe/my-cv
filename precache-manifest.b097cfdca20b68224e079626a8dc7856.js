c999ee806c4af02e7f6d1dda883c4d2241b7ab6d
Sergei Mikhailovskikh <s.mikhailovskikh@volsor.com>
2019-06-29 11:06:31 +0200

    Updates

---
+++ precache-manifest.b097cfdca20b68224e079626a8dc7856.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d2aa066042be8b71947b29f4885305",
+    "url": "my-cv/index.html"
+  },
+  {
+    "revision": "f18cd7086e322ce1c829",
+    "url": "my-cv/static/css/main.f3184091.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "my-cv/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "f18cd7086e322ce1c829",
+    "url": "my-cv/static/js/main.0fc88729.chunk.js"
+  },
+  {
+    "revision": "b816d53b8212a1aa4ef5",
+    "url": "my-cv/static/js/runtime~main.e4f31113.js"
+  }
+]);
